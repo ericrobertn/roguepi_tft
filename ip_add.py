ad72f9020ed7bec2fbafb02e75145b57bcf2ffb2
ericrobertn <ericneudorfer@gmail.com>
2014-11-25 15:51:31 -0800

    Update ip_add.py

--- ip_add.py
+++ ip_add.py
@@ -1,3 +1,12 @@
+#! /usr/bin/env python
+
+from pygame.locals import *
+from subprocess import call
+from time import sleep
+from sys import exit
+from ConfigParser import SafeConfigParser
+
+
 import smbus
 import subprocess
 import re
@@ -9,11 +18,7 @@ import signal
 import sys, pygame
 import time
 import os
-from pygame.locals import *
-from subprocess import call
-from time import sleep
-from sys import exit
-from ConfigParser import SafeConfigParser
+
 
 os.environ["SDL_FBDEV"] = "/dev/fb1"
 os.environ["SDL_MOUSEDEV"] = "/dev/input/touchscreen"
@@ -32,32 +37,36 @@ def get_ip_address(ifname):
                 0x8915,  # SIOCGIFADDR
                 struct.pack('256s', ifname[:15])
         )[20:24])
-		
-		
-# def dhcp_check():
-	# if int(ip_address = get_ip_address(iface)) > 0:
-		# font=pygame.font.Font(None,24)
-		# label=font.render("Failure", 1, (red))
-		# screen.blit(label,(40,120))
-		# pygame.display.flip()
-		# #print packet_loss + " packet loss."
-	# else:
-		# font=pygame.font.Font(None,24)
-		# label=font.render("Success", 1, (green))
-		# screen.blit(label,(40,120))
-		# pygame.display.flip()
 
-		
+
+# def dhcp_check():
+        # if int(ip_address = get_ip_address(iface)) > 0:
+                # font=pygame.font.Font(None,24)
+                # label=font.render("Failure", 1, (red))
+                # screen.blit(label,(40,120))
+                # pygame.display.flip()
+                # #print packet_loss + " packet loss."
+        # else:
+                # font=pygame.font.Font(None,24)
+                # label=font.render("Success", 1, (green))
+                # screen.blit(label,(40,120))
+                # pygame.display.flip()
+
+
 # Function which gets the Default Gateway IP address
 def get_gateway(ifname):
     proc = subprocess.Popen("ip route list dev " + ifname + " | awk ' /^default/ {print $3}'", \
-	shell=True, stderr=subprocess.PIPE, stdout=subprocess.PIPE)
+    shell=True, stderr=subprocess.PIPE, stdout=subprocess.PIPE)
     return_code = proc.wait()
     for line in proc.stdout:
         line
     return line
 
-		
+def make_stext(text, xpo, ypo, colour):
+    font=pygame.font.Font(None,18)
+    label=font.render(str(text), 1, (colour))
+    screen.blit(label,(xpo,ypo))
+
 def make_text(text, xpo, ypo, colour):
     font=pygame.font.Font(None,24)
     label=font.render(str(text), 1, (colour))
@@ -67,58 +76,69 @@ def make_button(text, xpo, ypo, colour):
     font=pygame.font.Font(None,24)
     label=font.render(str(text), 1, (colour))
     screen.blit(label,(xpo,ypo))
-	
-	#Ping Google Check
-def ping_google():	
-	proc = subprocess.Popen("ping -c 2 google.com 2>&1", \
-			shell=True, stderr=subprocess.PIPE, stdout=subprocess.PIPE)
- 
-	return_code = proc.wait()
- 
-	# Read from pipes
-	# stdout
-	for line in proc.stdout:
-		if "loss" in line:
-			packet_loss = progress = re.search('\d*%',line).group()
-			if int(packet_loss.split('%')[0]) > 0:
-				font=pygame.font.Font(None,24)
-				label=font.render("Failure", 1, (red))
-				screen.blit(label,(40,120))
-				pygame.display.flip()
-				#print packet_loss + " packet loss."
-			else:
-				font=pygame.font.Font(None,24)
-				label=font.render("Success", 1, (green))
-				screen.blit(label,(40,120))
-				pygame.display.flip()
 
-	#Ping Gateway
+        #Ping Google Check
+def ping_google():
+        proc = subprocess.Popen("ping -c 2 google.com 2>&1", \
+                        shell=True, stderr=subprocess.PIPE, stdout=subprocess.PIPE)
+
+        return_code = proc.wait()
+
+        # Read from pipes
+        # stdout
+        for line in proc.stdout:
+                if "loss" in line:
+                        packet_loss = progress = re.search('\d*%',line).group()
+                        if int(packet_loss.split('%')[0]) > 0:
+                                font=pygame.font.Font(None,24)
+                                label=font.render("Failure", 1, (red))
+                                screen.blit(label,(40,120))
+                                pygame.display.flip()
+                                #print packet_loss + " packet loss."
+                        else:
+                                font=pygame.font.Font(None,24)
+                                label=font.render("Success", 1, (green))
+                                screen.blit(label,(40,120))
+                                pygame.display.flip()
+
+        #Ping Gateway
 def ping_gateway():
-	ip_gateway = get_gateway(iface)
- 
-	proc = subprocess.Popen ("ping -c 2 " + ip_gateway + " 2>&1", \
-			shell=True, stderr=subprocess.PIPE, stdout=subprocess.PIPE)
- 
-	return_code = proc.wait()
- 
-	# Read from pipes
-	# stdout
-	for line in proc.stdout:
-	   if "loss" in line:
-		   packet_loss = re.search('\d*%',line).group()
-		   if int(packet_loss.split('%')[0]) > 0:
-			   	font=pygame.font.Font(None,24)
-				label=font.render("Failure")
-				screen.blit(label,(180,80))
-				pygame.display.flip()
-			   #print ip_gateway + packet_loss + " packet loss for gateway." 
-		   else:
-			font=pygame.font.Font(None,24)
-			label=font.render("Success", 1, (green))
-			screen.blit(label,(180,80))
-			pygame.display.flip()
-			   #print "Gateway is reachable"  
-				
+        ip_gateway = get_gateway(iface)
+
+        proc = subprocess.Popen ("ping -c 2 " + ip_gateway + " 2>&1", \
+                        shell=True, stderr=subprocess.PIPE, stdout=subprocess.PIPE)
+
+        return_code = proc.wait()
+
+        # Read from pipes
+        # stdout
+        for line in proc.stdout:
+           if "loss" in line:
+                   packet_loss = re.search('\d*%',line).group()
+                   if int(packet_loss.split('%')[0]) > 0:
+                                font=pygame.font.Font(None,24)
+                                label=font.render("Failure")
+                                screen.blit(label,(180,80))
+                                pygame.display.flip()
+                           #print ip_gateway + packet_loss + " packet loss for gateway."
+                   else:
+                        font=pygame.font.Font(None,24)
+                        label=font.render("Success", 1, (green))
+                        screen.blit(label,(180,80))
+                        pygame.display.flip()
+                           #print "Gateway is reachable"
+
+
+#Get Number of hosts from Nmap scan run at ./installer.sh
+def number_hosts():
+    proc = subprocess.Popen("cat /pentest/dumps/nmap-testinit | grep -o -P '.{0,20}hosts.{0,20}'", \
+        shell=True, stderr=subprocess.PIPE, stdout=subprocess.PIPE)
+    return_code = proc.wait()
+    for line in proc.stdout:
+        line
+    return line
+
+
 
 
 
@@ -138,10 +158,10 @@ screen = pygame.display.set_mode(size)
 #welcome screen
 screen.fill(black)
 font=pygame.font.Font(None,36)
-label=font.render("Rogue Pi", 1, (red))
+label=font.render("Rogue Pi_", 1, (red))
 screen.blit(label,(60,80))
 pygame.display.flip()
-time.sleep(1)
+time.sleep(5)
 pygame.display.update()
 
 screen.fill(black)
@@ -156,22 +176,23 @@ screen.fill(black)
 
 #This is where the text goes
 make_button("IP Address:", 20, 20, white)
-make_text(get_ip_address(iface),40,40,green) 
+make_text(get_ip_address(iface),40,40,green)
 
 make_button("Defualt Gateway:     Pinging Gateway",20,60,white)
-make_text(get_gateway(iface),40,80,green)      
+make_text(get_gateway(iface),40,80,green)
 print ping_gateway()
 
 
 make_button("Pinging Google",20, 100,white)
-print ping_google() 
+print ping_google()
 
 make_button("Secure Tunnel",20, 140,white)
-make_text(get_ip_address(ham),40,160,green) 
+make_text(get_ip_address(ham),40,160,green)
 
 make_button("Number of Hosts on Network:", 20, 180, white)
-make_text("Insert Nmap scan here",40,200,green)
+make_stext(number_hosts(),20,210,blue)
 
+make_text("RoguePi_,200,10,red)
 pygame.display.update()
 
 
@@ -190,3 +211,4 @@ while 1:
             if event.key == K_ESCAPE:
                 sys.exit()
     pygame.display.update()
+
